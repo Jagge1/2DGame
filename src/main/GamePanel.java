d1fe004ed9983acb6410be58d922fd3c74cc360d
Jacob <hellgrenjacob@hotmail.com>
2024-11-05 16:40:08 +0100

    Added a new class, KeyHandler to handle key input from user and som modifications to Main and GamePanel.

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -15,11 +15,21 @@ public class GamePanel extends JPanel implements Runnable{
     final int screenWidth = tileSize * maxScreenCol; // 768 pixels
     final int screenHeight = tileSize * maxScreenRow; // 576 pixels
 
+    KeyHandler keyHandler = new KeyHandler();
+
     Thread gameThread;
+
+    //Setting the players default position
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
+
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyHandler);
+        this.setFocusable(true);
     }
 
     public void startGameThread(){
@@ -33,8 +43,36 @@ public class GamePanel extends JPanel implements Runnable{
         while (gameThread != null){
 
             // 1. Update information (character positions)
+            update();
 
             // 2. Draw the screen
+            //paintComponent method is being called through repaint
+            repaint();
         }
+    }
+    public void update(){
+
+        if(keyHandler.upPressed == true){
+            playerY -= playerSpeed;
+        } else if (keyHandler.downPressed == true){
+            playerY += playerSpeed;
+        } else if (keyHandler.leftPressed == true){
+            playerX -= playerSpeed;
+        } else if (keyHandler.rightPressed == true) {
+            playerX += playerSpeed;
+        }
+
+    }
+    //JPanel method
+    public void paintComponent(Graphics g){
+
+        super.paintComponent(g);
+
+        //Changed Graphics g to Graphics2D g2 for extra functionality
+        Graphics2D g2 = (Graphics2D)g;
+
+        g2.setColor(Color.white);
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
+        g2.dispose();
     }
 }
